98b6b86636c0f0023e755bf90657261453f50f4e
Joel Grayson <joel@joelgrayson.com>
2023-07-04 18:54:47 -0400

    One stepper motor

---
+++ README.md
@@ -0,0 +1,4 @@
+# Healthy Vending Machine
+Powered by Arduino connected to bill acceptor, 8 stepper motors, LCD, and membrane keypad.
+
+
--- healthy_vending_machine.ino
+++ healthy_vending_machine.ino
@@ -24,6 +24,17 @@ byte colPins[N_COLS]={ 5, 4, 3 };
 Keypad keypad=Keypad(makeKeymap(keys), rowPins, colPins, N_ROWS, N_COLS);
 
 
+// # Stepper Motor
+#include <Stepper.h>
+#define STEPS_PER_REVOLUTION 32
+#define GEAR_REDUCTION 64
+const float STEPS_PER_OUTPUT_REVOLUTION=STEPS_PER_REVOLUTION*GEAR_REDUCTION;
+const float tau=STEPS_PER_OUTPUT_REVOLUTION; //one full revolution
+// byte pins[4]={13, 12, 11, 10};
+byte pins[4]={46, 48, 50, 52};
+Stepper motor(STEPS_PER_REVOLUTION, pins[0], pins[2], pins[1], pins[3]);  //Pin mapping: 1N1 -> 13, 1N2 -> 12, 1N3 -> 11, 1N4 -> 10
+
+
 void setup() {
     Serial.begin(9600);
     Serial.println("Serial began");
@@ -36,16 +47,22 @@ void setup() {
 
 
 void loop() {
-    // Get value if keypad pressed
-    char customKey=keypad.getKey();
+    // // Get value if keypad pressed
+    // char customKey=keypad.getKey();
     
-    if (customKey) { //pressed key
-        char message[50]; //buffer
-        sprintf(message, "Selected %c.", customKey);
-        Serial.println(message);
+    // if (customKey) { //pressed key
+    //     char message[50]; //buffer
+    //     sprintf(message, "Selected %c.", customKey);
+    //     Serial.println(message);
 
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print(message);
-    }
+    //     lcd.clear();
+    //     lcd.setCursor(0, 0);
+    //     lcd.print(message);
+    // }
+
+    Serial.println("Stepping");
+    motor.setSpeed(1000);
+    motor.step(tau);
+    Serial.println("Finished stepping");
+    delay(1500);
 }
