35b775d007e810019d76bd0a59f9fcff91f2973e
Joel Grayson <joel@joelgrayson.com>
2023-09-21 20:26:33 -0400

    keypad triggers DC motors for springs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Healthy Vending Machine
-Powered by Arduino connected to bill acceptor, 8 stepper motors, LCD, and membrane keypad.
+Powered by Arduino connected to bill acceptor, 8 DC motors, LCD, and membrane keypad.
 
 
--- healthy_vending_machine.ino
+++ healthy_vending_machine.ino
@@ -1,6 +1,5 @@
 // # Global Constants
-// #define N_SNACKS 8
-#define N_SNACKS 2
+#define N_SNACKS 8
 
 
 // # Liquid Crystal Display
@@ -9,9 +8,9 @@
 LiquidCrystal_I2C lcd(0x27, 20, 4); //20x4 display from amazon.com/gp/product/B01DKETWO2
     // Pin 9–13
 
+
 // # Keypad
-// Mark Stanley's Keypad library
-#include <Keypad.h>
+#include <Keypad.h> //Mark Stanley's Keypad library
 
 const uint8_t N_ROWS=4;
 const uint8_t N_COLS=3;
@@ -28,38 +27,30 @@ uint8_t colPins[N_COLS]={ 5, 4, 3 };
 
 Keypad keypad(makeKeymap(keys), rowPins, colPins, N_ROWS, N_COLS);
 
+
 // # Dollar Acceptor
 // Each pulse is configured to be one dollar. From https://github.com/mudmin/AnotherMaker/blob/master/arduino-powered-cash-reader/cash-reader-with-lcd.c
 #define dollarPin 15 //blue pin. Purple pin is connected to ground
 // It is HIGH when normal. When $ inserted, it beeps LOW for ~100 millis per dollar
-int pulse=HIGH;
-int prevPulse=HIGH;
+uint8_t pulse=HIGH;
+uint8_t prevPulse=HIGH;
 double moneyInserted=0;
 
 
-// # Stepper Motor
-#include <Stepper.h>
-Stepper* motors=malloc(sizeof(Stepper)*N_SNACKS);
-
-uint8_t pins[N_SNACKS][4]={
-    {46, 48, 50, 52}, //Pin mapping: 1N1 -> 46, 1N2 -> 48, 1N3 -> 50, 1N4 -> 52
-    {47, 49, 51, 53},
-    // {0, 0, 0, 0},
-    // {0, 0, 0, 0},
-    // {0, 0, 0, 0},
-    // {0, 0, 0, 0},
-    // {0, 0, 0, 0},
-    // {0, 0, 0, 0},
-};
-
-#define STEPS_PER_REVOLUTION 32
-#define GEAR_REDUCTION 64
-const float STEPS_PER_OUTPUT_REVOLUTION=STEPS_PER_REVOLUTION*GEAR_REDUCTION;
-const float tau=STEPS_PER_OUTPUT_REVOLUTION; //one full revolution
+// # DC Motor
+const uint8_t motorPins[N_SNACKS]={ 23, 25, 27, 29, 31, 33, 35, 37 }; //23–37 even numbers
+void turnOnce(uint8_t motorI) { //turns the transistor controlling the spring motor just long enough for one revolution
+    const int transistorPin=motorPins[motorI];
+    Serial.print("Turning "); Serial.println(transistorPin);
+    digitalWrite(transistorPin, HIGH);
+    delay(3300);
+    digitalWrite(transistorPin, LOW);
+}
 
 
 void setup() {
     Serial.begin(9600);
+    while (!Serial);
     Serial.println("Serial began");
     
     lcd.init();
@@ -67,38 +58,34 @@ void setup() {
     lcd.clear();
     lcd.setCursor(0, 0);
 
-    for (int i=0; i<N_SNACKS; i++)
-        motors[i]=Stepper(STEPS_PER_REVOLUTION, pins[0][0], pins[0][2], pins[0][1], pins[0][3]);
-    
 
     // Bill Acceptor
     pinMode(dollarPin, INPUT_PULLUP);
-}
 
+    // DC Motor
+    for (uint8_t motorPin : motorPins)
+        pinMode(motorPin, OUTPUT);
+}
 
 void loop() {
     // Keypad
     char customKey=keypad.getKey(); //get value if keypad pressed
     
     if (customKey) { //key pressed
-        uint8_t motorI=int(customKey)-int('1'); //offset from 1
+        uint8_t motorI=(uint8_t) (int(customKey)-int('1')); //offset from 1
+
+        // Show message
         lcd.clear();
         lcd.setCursor(0, 0);
+        if (motorI>N_SNACKS-1) return lcd.print("Out of range"); //validate customKey in range
 
-        if (motorI>N_SNACKS-1) { //validate customKey in range
-            return lcd.print("Out of range");
-        }
-        
         char message[50]; //buffer
         sprintf(message, "Selected %c.", customKey);
+        lcd.print(message);
         Serial.println(message);
 
-        lcd.print(message);
-
-        Serial.print("motorI"); Serial.println(motorI);
-        
-        motors[motorI].setSpeed(1000);
-        motors[motorI].step(tau);
+        // Turn
+        turnOnce(motorI);
     }
 
     // Bill Acceptor
